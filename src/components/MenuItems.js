3a9082b3b1593369af5c49ce2ed402eb8a320094
Rodrigo Collins <rodri_collins@hotmail.com>
2021-04-13 16:26:28 -0300

    https://github.com/RodrigoCollins/e-commerce-collins

---
+++ src/components/MenuItems.js
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+
+export const links = [
+  {
+    id: 1,
+    url: '#',
+    text: 'Home',
+  },
+  {
+    id: 2,
+    url: '#',
+    text: 'Men',
+  },
+  {
+    id: 3,
+    url: '#',
+    text: 'Women',
+  },
+  {
+    id: 4,
+    url: '#',
+    text: 'Kids',
+  },
+  {
+    id: 5,
+    url: '#',
+    text: 'Gifts',
+  },
+   {
+    id: 6,
+    url: '#',
+    text: <FontAwesomeIcon icon={faCartPlus} />,
+  },
+];
